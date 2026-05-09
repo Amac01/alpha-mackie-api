e405b2fdc3ab3441952d6b4c3e2a93ebe9ef9e73
Alpha Mackie <alphamackie@Alphas-MacBook-Pro.local>
2026-05-09 13:56:36 +0100

    Updated  the code for packageController file

--- server/Controllers/packageController.js
+++ server/Controllers/packageController.js
@@ -1,27 +1,35 @@
 import Package from "../models/Package.js";
 
+
+// CREATE PACKAGE
 export const createPackage = async (req, res) => {
+
+  console.log("BODY:", req.body);
+  console.log("USER:", req.user);
+
   try {
 
-    const { description, weight, length, width, height } = req.body;
-
     const newPackage = await Package.create({
-      userId: req.user.id,
-      description,
-      weight,
-      length,
-      width,
-      height
+      ...req.body,
+      userId: req.user.id
     });
 
     res.status(201).json(newPackage);
 
   } catch (error) {
-    res.status(500).json({ message: error.message });
+
+    console.log("PACKAGE ERROR:", error);
+
+    res.status(500).json({
+      message: error.message
+    });
   }
 };
 
+
+// GET USER PACKAGES
 export const getMyPackages = async (req, res) => {
+
   try {
 
     const packages = await Package.find({
@@ -31,34 +39,49 @@ export const getMyPackages = async (req, res) => {
     res.json(packages);
 
   } catch (error) {
-    res.status(500).json({ message: error.message });
+
+    res.status(500).json({
+      message: error.message
+    });
   }
 };
 
-export const approvePackage = async (req, res) => {
-  try {
 
-    const { price } = req.body;
+// APPROVE PACKAGE
+export const approvePackage = async (req, res) => {
+
+  try {
 
     const pkg = await Package.findById(req.params.id);
 
     if (!pkg) {
-      return res.status(404).json({ message: "Package not found" });
+      return res.status(404).json({
+        message: "Package not found"
+      });
     }
 
-    pkg.price = price;
     pkg.status = "approved";
+
+    if (req.body.price) {
+      pkg.price = req.body.price;
+    }
 
     await pkg.save();
 
     res.json(pkg);
 
   } catch (error) {
-    res.status(500).json({ message: error.message });
+
+    res.status(500).json({
+      message: error.message
+    });
   }
 };
 
+
+// GET ALL PACKAGES
 export const getAllPackages = async (req, res) => {
+
   try {
 
     const packages = await Package.find()
@@ -67,17 +90,25 @@ export const getAllPackages = async (req, res) => {
     res.json(packages);
 
   } catch (error) {
-    res.status(500).json({ message: error.message });
+
+    res.status(500).json({
+      message: error.message
+    });
   }
 };
 
+
+// MARK RECEIVED
 export const markReceived = async (req, res) => {
+
   try {
 
     const pkg = await Package.findById(req.params.id);
 
     if (!pkg) {
-      return res.status(404).json({ message: "Package not found" });
+      return res.status(404).json({
+        message: "Package not found"
+      });
     }
 
     pkg.status = "received";
@@ -87,14 +118,26 @@ export const markReceived = async (req, res) => {
     res.json(pkg);
 
   } catch (error) {
-    res.status(500).json({ message: error.message });
+
+    res.status(500).json({
+      message: error.message
+    });
   }
 };
 
+
+// MARK SHIPPED
 export const markShipped = async (req, res) => {
+
   try {
 
     const pkg = await Package.findById(req.params.id);
+
+    if (!pkg) {
+      return res.status(404).json({
+        message: "Package not found"
+      });
+    }
 
     pkg.status = "shipped";
 
@@ -103,14 +146,26 @@ export const markShipped = async (req, res) => {
     res.json(pkg);
 
   } catch (error) {
-    res.status(500).json({ message: error.message });
+
+    res.status(500).json({
+      message: error.message
+    });
   }
 };
 
+
+// MARK DELIVERED
 export const markDelivered = async (req, res) => {
+
   try {
 
     const pkg = await Package.findById(req.params.id);
+
+    if (!pkg) {
+      return res.status(404).json({
+        message: "Package not found"
+      });
+    }
 
     pkg.status = "delivered";
 
@@ -119,20 +174,9 @@ export const markDelivered = async (req, res) => {
     res.json(pkg);
 
   } catch (error) {
-    res.status(500).json({ message: error.message });
+
+    res.status(500).json({
+      message: error.message
+    });
   }
-};
-
-export const markPaid = async (packageId) => {
-  const pkg = await Package.findById(packageId);
-
-  if (!pkg) {
-    throw new Error("Package not found");
-  }
-
-  pkg.status = "paid";
-
-  await pkg.save();
-
-  return pkg;
 };
