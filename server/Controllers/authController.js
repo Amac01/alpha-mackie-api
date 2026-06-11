ff9fef5679ba774e517577c97e648e178be463af
Amac01 <lphmackie@gmail.com>
2026-06-11 20:47:41 +0100

    Add forgot password and reset password functionality

--- server/Controllers/authController.js
+++ server/Controllers/authController.js
@@ -86,3 +86,39 @@ try {
 
   }
 };
+export const resetPassword = async (req, res) => {
+  try {
+
+    const { token, password } = req.body;
+
+    const user = await User.findOne({
+      resetPasswordToken: token,
+      resetPasswordExpires: { $gt: Date.now() }
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid or expired reset token"
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user.password = hashedPassword;
+
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpires = undefined;
+
+    await user.save();
+
+    res.json({
+      message: "Password reset successful"
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: error.message
+    });
+  }
+};
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,12 +1,13 @@
 // routes/authRoutes.js
 
 import express from "express";
-import { register, login, forgotPassword } from "../Controllers/authController.js";
+import { register, login, forgotPassword, resetPassword } from "../Controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
 
 export default router;
